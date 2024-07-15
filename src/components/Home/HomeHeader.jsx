f75af1165a147a0af1acad8995d6e30b96069806
Patryk Krawczyk <patrykkrawczykk157@gmail.com>
2024-07-15 18:49:25 +0200

    initial commit

---
+++ src/components/Home/HomeHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeHeader = () => {
+  return (
+    <header>
+      <h1>Welcome to the Home Page</h1>
+    </header>
+  );
+};
+
+export default HomeHeader;
---
+++ src/components/Home/HomeThreeColumns.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomeThreeColumns = () => {
+  return (
+    <div>
+      <div>Column 1</div>
+      <div>Column 2</div>
+      <div>Column 3</div>
+    </div>
+  );
+};
+
+export default HomeThreeColumns;
