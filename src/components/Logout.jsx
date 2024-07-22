d98ca691b8be39dc135d6b4b5e2633167ee2fecf
Patryk Krawczyk <patrykkrawczykk157@gmail.com>
2024-07-22 21:19:12 +0200

    logout section

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import HomeHeader from "./Home/HomeHeader";
+import { Link } from "react-router-dom";
 const Logout = () => {
   return (
     <>
       <HomeHeader />
-      <h2>Wylogowano</h2>
+      <div className="logout">
+        <div className="logout-container">
+          <h2 className="logout-title">
+            Wylogowanie nastąpiło
+            <br />
+            pomyślnie!
+          </h2>
+          <div className="header-border"></div>
+          <button className="btn-home">
+            <Link to="/">Strona główna</Link>
+          </button>
+        </div>
+      </div>
     </>
   );
 };
