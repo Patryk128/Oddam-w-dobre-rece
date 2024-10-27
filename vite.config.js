8109d619481943a0fba687d3609b6286033688ff
Patryk128 <145597315+Patryk128@users.noreply.github.com>
2024-10-27 16:40:14 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Umożliwia udostępnienie hosta w sieci lokalnej
-    open: true, // Automatycznie otwiera aplikację w przeglądarce
+    host: true, 
+    open: true, 
   },
 });
